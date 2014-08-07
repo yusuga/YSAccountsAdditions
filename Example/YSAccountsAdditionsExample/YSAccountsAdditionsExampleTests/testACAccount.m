508a7313a771a45cb848aa0e8f9c77def5255f3f
Yu Sugawara <yusuga@me.com>
2014-08-07 18:12:14 +0900

    tweak Tests

--- Example/YSAccountsAdditionsExample/YSAccountsAdditionsExampleTests/testACAccount.m
+++ Example/YSAccountsAdditionsExample/YSAccountsAdditionsExampleTests/testACAccount.m
@@ -20,12 +20,11 @@
 - (void)setUp
 {
     [super setUp];
-    // Put setup code here. This method is called before the invocation of each test method in the class.
+    [TKRGuard setDefaultTimeoutInterval:30.];
 }
 
 - (void)tearDown
 {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
     [super tearDown];
 }
 
@@ -37,7 +36,9 @@
         XCTAssertTrue([accounts count] > 0);
         
         for (ACAccount *account in accounts) {
+            id userID = [account ys_twitterUserID];
             XCTAssertNotNil([account ys_twitterUserID]);
+            XCTAssertTrue([userID isKindOfClass:[NSString class]], @"class: %@", NSStringFromClass([userID class]));
         }
         RESUME;
     }];
